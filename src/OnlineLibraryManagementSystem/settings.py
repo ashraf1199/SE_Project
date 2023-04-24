fd314c7cb3d9e26a5cc9f6c8962198d7908f971b
AbhijithMandagiri <123501561+AbhijithMandagiri@users.noreply.github.com>
2023-04-24 15:13:35 -0500

    Updated settings

--- src/OnlineLibraryManagementSystem/settings.py
+++ src/OnlineLibraryManagementSystem/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-!^k@9kpne36bz*dy+-gk%*ae!i$ryd54%fd2assek7c-pk^dav
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1','onlinelibrarymanagementsystem.herokuapp.com']
 
 
 # Application definition
